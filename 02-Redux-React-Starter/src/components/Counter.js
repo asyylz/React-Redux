21d2b86292b4dd8614f62b5126cff6f99ae841e4
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-04 16:17:53 +0100

    access to  counter data managed by Redux.

--- 02-Redux-React-Starter/src/components/Counter.js
+++ 02-Redux-React-Starter/src/components/Counter.js
@@ -1,12 +1,20 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { UseSelector, useSelector } from "react-redux";
 
 const Counter = () => {
+  // NOTES
+  // When you use use selector, React Redux will automatically set up a subscription to the Redux store for this component.
+
+  // If you ever would unmount this component if it would be removed from the DOM for whatever reason, React Redux would also automatically clear the subscription for you. So it manages that subscription for you behind the scenes.
+
+  const counter = useSelector((state) => state.counter); // this function will be executed for us by React Redux.
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
