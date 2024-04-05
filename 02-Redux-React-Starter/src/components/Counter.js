8d64211496bac07e5826f7c8ab8b2a0b49b76a60
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-05 12:41:05 +0100

    class based component

--- 02-Redux-React-Starter/src/components/Counter.js
+++ 02-Redux-React-Starter/src/components/Counter.js
@@ -1,37 +1,78 @@
 import classes from "./Counter.module.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, connect } from "react-redux";
+import { Component } from "react";
+// const Counter = () => {
+//   // dispatch function, a function which we can call, which will dispatch an action against our Redux store.
+//   const dispatch = useDispatch();
 
-const Counter = () => {
-  // dispatch function, a function which we can call, which will dispatch an action against our Redux store.
-  const dispatch = useDispatch();
+//   const incrementHandler = () => {
+//     dispatch({ type: "increment" });
+//   };
+//   const decrementHandler = () => {
+//     dispatch({ type: "decrement" });
+//   };
 
-  const incrementHandler = () => {
-    dispatch({ type: "increment" });
+//   // NOTES
+//   // When you use use selector, React Redux will automatically set up a subscription to the Redux store for this component.
+
+//   // If you ever would unmount this component if it would be removed from the DOM for whatever reason, React Redux would also automatically clear the subscription for you. So it manages that subscription for you behind the scenes.
+
+//   const counter = useSelector((state) => state.counter); // this function will be executed for us by React Redux.
+
+//   const toggleCounterHandler = () => {};
+
+//   return (
+//     <main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       <div className={classes.value}>{counter}</div>
+//       <div className="counter">
+//         <button onClick={incrementHandler}>Increment</button>
+//         <button onClick={decrementHandler}>Decrement</button>
+//       </div>
+//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//     </main>
+//   );
+// };
+
+//export default Counter;
+
+class Counter extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
   };
-  const decrementHandler = () => {
-    dispatch({ type: "decrement" });
-  };
-
-  // NOTES
-  // When you use use selector, React Redux will automatically set up a subscription to the Redux store for this component.
-
-  // If you ever would unmount this component if it would be removed from the DOM for whatever reason, React Redux would also automatically clear the subscription for you. So it manages that subscription for you behind the scenes.
-
-  const counter = useSelector((state) => state.counter); // this function will be executed for us by React Redux.
-
-  const toggleCounterHandler = () => {};
-
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div className="counter">
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
 };
 
-export default Counter;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
