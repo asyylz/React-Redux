9000319bdc042967e8e42f1b9f5cf5e8d4c7d779
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-05 13:44:58 +0100

    redux toolkit

--- 02-Redux-React-Starter/src/store/index.js
+++ 02-Redux-React-Starter/src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counter++;
+    },
+    decrement: (state) => {
+      state.counter--;
+    },
+    increase: (state, action) => {
+      state.counter = state.counter + action.amount;
+    },
+    toggle: (state) => {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
@@ -37,3 +58,9 @@ const counterReducer = (state = initialState, action) => {
 const store = createStore(counterReducer);
 
 export default store;
+
+// NOTES:
+//reducer function always return a brand new snapshot, a brand new state object, which Redux will use to replace its existing state with. So the objects which we return in the reducer will not, and that's super important, be merged with the existing state. They will overwrite the existing state.
+
+//Crucial:
+//You should never super important never mutate the state, the existing state.You should never change the existing state.Instead, always override it by returning a brand new state object.
