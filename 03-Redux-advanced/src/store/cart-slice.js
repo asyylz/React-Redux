64dd21abaf54fa34d7f0a5a0a13ef8187a289bb6
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-09 15:41:48 +0100

    update

--- 03-Redux-advanced/src/store/cart-slice.js
+++ 03-Redux-advanced/src/store/cart-slice.js
@@ -9,9 +9,7 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(
-        (item) => item.id === newItem.title
-      );
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -25,10 +23,19 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
 export const cartActions = cartSlice.actions;
 
-export default cartSlice.reducer;
+export default cartSlice;
