386f343d79888254eb10abab2b1eb64aa7a8cd6c
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-10 00:26:03 +0100

    update

--- 04-Redux-advanced -backend/src/App.js
+++ 04-Redux-advanced -backend/src/App.js
@@ -1,15 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
+let isInitial = true;
 function App() {
   const dispatch = useDispatch();
 
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
@@ -29,7 +32,10 @@ function App() {
       }
       const responseData = await response.json();
     };
-
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
     sendCartData().catch((error) =>
       dispatch(
         uiActions.showNotification({
@@ -42,10 +48,19 @@ function App() {
   }, [cart, dispatch]); //React Redux will ensure this function which will never change, only cart changes will.
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          title={notification.title}
+          status={notification.status}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
