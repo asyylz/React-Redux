98deb09bd2d06df07bacedb67ddbd289852531b4
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-10 00:04:25 +0100

    notification

--- 04-Redux-advanced -backend/src/App.js
+++ 04-Redux-advanced -backend/src/App.js
@@ -1,19 +1,45 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+  const dispatch = useDispatch();
+
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch(
-      "https://shopping-cart-http-2134d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully",
+        })
+      );
+      const response = await fetch(
+        "https://shopping-cart-http-2134d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+      const responseData = await response.json();
+    };
+
+    sendCartData().catch((error) =>
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      )
     );
-  }, [cart]);
+  }, [cart, dispatch]); //React Redux will ensure this function which will never change, only cart changes will.
 
   return (
     <Layout>
--- 04-Redux-advanced -backend/src/store/ui-slice.js
+++ 04-Redux-advanced -backend/src/store/ui-slice.js
@@ -2,10 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible; //we can write this mutating code here  because you learned that when using Redux Toolkit,we are not really mutating the state,
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
